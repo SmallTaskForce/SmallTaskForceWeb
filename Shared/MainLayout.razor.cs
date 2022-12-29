c9a4f163de0782543b00f5a9a8bb09367884da9d
skiddow <achinsorry@gmail.com>
2022-12-29 13:51:03 +0530

    .

---
+++ Shared/MainLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿using MudBlazor;
+
+namespace SmallTaskForceWeb.Shared;
+
+public partial class MainLayout
+{
+    private bool _isDarkMode;
+    private MudThemeProvider _mudThemeProvider;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+            StateHasChanged();
+        }
+    }
+
+    bool _drawerOpen = true;
+
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+}
