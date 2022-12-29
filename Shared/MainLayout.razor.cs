91b25880627b025e7200e63f7fe7d0ddedc540a7
skiddow <achinsorry@gmail.com>
2022-12-29 19:59:04 +0530

    .

--- Shared/MainLayout.razor.cs
+++ Shared/MainLayout.razor.cs
@@ -1,19 +1,21 @@
-﻿using MudBlazor;
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace SmallTaskForceWeb.Shared;
 
 public partial class MainLayout
 {
+    private MudTheme _theme = new();
     private bool _isDarkMode;
     private MudThemeProvider _mudThemeProvider;
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        if (firstRender)
-        {
-            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
-            StateHasChanged();
-        }
+        //if (firstRender)
+        //{
+        //    _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+        //    StateHasChanged();
+        //}
     }
 
     bool _drawerOpen = true;
