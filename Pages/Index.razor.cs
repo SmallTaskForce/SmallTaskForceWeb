6f89da3bcb8caa5dcbd7c5f2daad0ddf310dd2be
skiddow <achinsorry@gmail.com>
2022-12-30 17:16:21 +0530

    .

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace SmallTaskForceWeb.Pages;
+
+public partial class Index
+{
+    public string Description = "At STF, we understand that life can be busy and hectic. That's why we offer smart home and automated solutions to help make life a little easier. Our team of expert developers also provides custom software development for businesses, aimed at increasing efficiency and streamlining processes. In addition, we offer mobile and web app development to bring the convenience of technology to your fingertips. Our ultimate goal is to make the lives of our customers better and their businesses more efficient.";
+}
